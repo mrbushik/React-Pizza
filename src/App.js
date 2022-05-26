bd283d8defd569871f51f8af5ab2a3a139d09422
Nikita <mrbushik1@gmail.com>
2022-05-26 22:24:40 +0300

    get button categories and render buttons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import logo from './assets/img/pizza-logo.svg';
-import Header from './Header'
-import Button from './Button'
+import { Header, Categories} from './components';
 
 function App() {
+
+
   return (
     <div className="wrapper">
   <Header/>
-  <Button outline/>
-  <Button />
     <div className="content">
       <div className="container">
         <div className="content__top">
-          <div className="categories">
-            <ul>
-              <li className="active">Все</li>
-              <li>Мясные</li>
-              <li>Вегетарианская</li>
-              <li>Гриль</li>
-              <li>Острые</li>
-              <li>Закрытые</li>
-            </ul>
-          </div>
+       <Categories
+       onClick={(name)=>{console.log(name)}}
+       items={[
+         'Мясные',
+         'Вегетарианские',
+         'Гриль',
+         'Острые',
+         '',
+       ]}/>
           <div className="sort">
             <div className="sort__label">
               <svg
