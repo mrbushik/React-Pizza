522117b87ce0c4d6435641422636ac0574c7e00d
Nikita <mrbushik1@gmail.com>
2022-05-29 19:06:39 +0300

    router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route } from 'react-router-dom';
+import {Home} from './pages/Home'
+import {Cart} from './pages/Cart'
+
 import './scss/app.scss'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+<App/>
+
+{/* <Route path="/" element={<App />} exact /> */}
+{/* <Route path="/cart" element={<Cart />} exact /> */}
+</BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cart() {
+  return (
+    <>
+    <h1>Корзина</h1>
+    </>
+  )
+}
+
+export default Cart
