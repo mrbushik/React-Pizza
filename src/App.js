8733484e30cd303faf54f0fc3eafde73703d083b
Nikita <mrbushik1@gmail.com>
2022-06-08 14:47:05 +0300

    adding redux hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import axios from 'axios';
-import {connect} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 
 import {setPizzas} from './redux/actions/pizzas'
 import { Header} from './components';
 import {Home, Cart} from './pages'
 import { Route, Routes } from 'react-router-dom';
 
-function App({setPizzas, items}) {
+function App() {
+const dispatch = useDispatch()
+const {items}= useSelector(({pizzas,filters}) => {
+  return{
+    items: pizzas.items,
+    sortBy: filters.sortBy
+  }
+});
+
+// console.log(state);
   React.useEffect(()=>{
     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      setPizzas(data.pizzas);
-    });
+dispatch(setPizzas(data.pizzas))
+  });
   }, [])
   return (
     <div className="wrapper">
@@ -26,16 +35,18 @@ function App({setPizzas, items}) {
   )
 }
 
-export default connect(
-  (state) => {
-    return {
-      items: state.pizzas.items,
-      filters: state.filters,
-    };
-  },
-  (dispatch) => {
-    return {
-      setPizzas: (items) => dispatch(setPizzas(items)),
-    };
-  },
-)(App);
+export default App
+
+// export default connect(
+//   (state) => {
+//     return {
+//       items: state.pizzas.items,
+//       filters: state.filters,
+//     };
+//   },
+//   (dispatch) => {
+//     return {
+//       setPizzas: (items) => dispatch(setPizzas(items)),
+//     };
+//   },
+// )(App);
